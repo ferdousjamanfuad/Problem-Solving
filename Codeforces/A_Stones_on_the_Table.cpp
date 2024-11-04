86a6a6bf06212963f6122cbcc642a95cf790fa8a
Fuadferdous <fuad.jaman.ferdous@gmail.com>
2024-11-04 18:04:22 +0600

    uploading first code

---
+++ Codeforces/A_Stones_on_the_Table.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <String>
+using namespace std;
+int main()
+{
+    int n, count = 0;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < s.length()-1; i++)
+    {
+        if (s[i] == s[i + 1])
+            count++;
+    }
+    cout << count;
+
+    return 0;
+}
